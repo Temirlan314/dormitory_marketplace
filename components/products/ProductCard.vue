9ed24d31f63513b199d63146bec12baad30010a1
temirlanprimtayev <temirlanprimtayev@gmail.com>
2023-11-14 01:10:11 +0600

    profile page product cards

---
+++ components/products/ProductCard.vue
@@ -0,0 +1,95 @@
+<
+<script>
+/**
+ * Product Card component
+ */
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+    currency: {
+      type: String,
+      required: false,
+      default: "Tenge",
+    },
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+<template>
+  <div class="card-vertical hoverable">
+    <img
+      src="~/assets/local_images/Image 2.png"
+      alt="No image"
+      style="width: 358px"
+    />
+    <div class="card-text">
+      <div class="product-price">{{ product.price }} {{ currency }}</div>
+      <div class="product-name">{{ product.name }}</div>
+      <div class="product-description">{{ product.description }}</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card-vertical {
+  border-radius: 8px;
+  border: 1px solid var(--tertiary-gray-200-light, #e4e4e7);
+  background: var(--base-00-light-primary, #fff);
+
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.08);
+
+  display: flex;
+  width: 360px;
+  height: 400px;
+  padding-bottom: 0px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 16px;
+}
+
+.card-text {
+  display: flex;
+  padding: 0px 24px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+  align-self: stretch;
+  font-size: 16px;
+  font-style: normal;
+  line-height: 24px;
+}
+
+.product-name {
+  color: var(--label-primary, #10083f);
+  font-feature-settings: "clig" off, "liga" off;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 28px;
+}
+
+.product-price {
+  color: rgba(26, 26, 26, 0.5);
+  font-weight: 600;
+  font-size: 16px;
+  font-style: normal;
+  line-height: 24px;
+}
+
+.product-description {
+  color: #1a1a1a;
+  font-weight: 400;
+  font-size: 16px;
+  font-style: normal;
+  line-height: 24px;
+}
+.hoverable:hover {
+  cursor: pointer;
+}
+</style>
--- pages/account/profile.vue
+++ pages/account/profile.vue
@@ -1,4 +1,6 @@
 <script>
+import ProductCard from "~/components/products/ProductCard.vue";
+
 /**
  * Login component
  */
@@ -14,10 +16,28 @@ export default {
       authError: null,
       tryingToLogIn: false,
       isAuthError: false,
+      posts: [
+        {
+          name: "QR hoodie",
+          price: "10 000",
+          description: "New hoodie",
+        },
+        {
+          name: "Murakami book",
+          price: "1 000",
+          description: "Used book",
+        },
+        {
+          name: "Mouse",
+          price: "45 000",
+          description: "New magic mouse. ",
+        },
+      ],
     };
   },
   computed: {},
   methods: {},
+  components: { ProductCard },
 };
 </script>
 <template>
@@ -26,43 +46,39 @@ export default {
       <b-avatar
         size="lg"
         class="bg-soft-danger text-dark font-20"
-        style="width: 112px;
-height: 112px;
-margin-right: 32px;
-"
-        ><span
-          style="font-size: 32px;
-"
-          >AA</span
-        ></b-avatar
+        style="width: 112px; height: 112px; margin-right: 32px;"
       >
+        <span style="font-size: 32px;">AA</span>
+      </b-avatar>
       <div class="text-dark">
-        <p
-          style="font-size: 32px;
-font-style: normal;
-font-weight: 600;
-line-height: normal;"
-        >
+        <p class="name-text">
           Alen Amina
         </p>
-        <p>amina.alen@nu.edu.kz</p>
-        <p>@lemontartaletka</p>
+        <div class="info-text">
+          <div style="margin-bottom: 8px;">amina.alen@nu.edu.kz</div>
+          <div>@lemontartaletka</div>
+        </div>
       </div>
     </div>
-    <div class="big-text hoverable">
+    <div class="big-text hoverable" style="margin-bottom: 32px;">
       My posts
-      <i
-        class="mdi mdi-chevron-right
-"
-      ></i>
+      <i class="mdi mdi-chevron-right"></i>
     </div>
+    <div class="d-flex">
+      <div v-for="post in posts" style="margin-right: 24px;">
+        <ProductCard :product="post" />
+      </div>
+    </div>
+
     <br /><br />
-    <div class="big-text hoverable">
+    <div class="big-text hoverable" style="margin-bottom: 32px;">
       My purchases
-      <i
-        class="mdi mdi-chevron-right
-"
-      ></i>
+      <i class="mdi mdi-chevron-right"></i>
+    </div>
+    <div class="d-flex">
+      <div v-for="post in posts" style="margin-right: 24px;">
+        <ProductCard :product="post" />
+      </div>
     </div>
   </div>
 </template>
@@ -71,6 +87,19 @@ line-height: normal;"
   color: #0d0d0d;
   font-size: 32px;
   font-weight: 600;
+  line-height: normal;
+}
+.name-text {
+  color: #0d0d0d;
+  font-size: 32px;
+  font-weight: 600;
+  line-height: normal;
+}
+.info-text {
+  color: #0d0d0d;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
   line-height: normal;
 }
 .hoverable:hover {
